a40a1a6f3bf4542bfcb60d921cc21d3563f539aa
peter <p.c.stevens@outlook.com>
2024-09-14 18:27:05 +0100

    Add elements dict

--- ElementalForms/ElementalForms.cs
+++ ElementalForms/ElementalForms.cs
@@ -1,9 +1,13 @@
-﻿namespace ElementalForms;
+﻿using static Preloaded.Elements;
+
+namespace ElementalForms;
 
 public class ElementalForms
 {
     public string[][] GetElementalForms(string word)
     {
+        string hydrogen = ELEMENTS["H"];
+
         return Array.Empty<string[]>();
     }
 }
